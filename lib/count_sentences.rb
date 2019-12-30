bf9e27f042b3f3bb108a8a02d5997672accce59c
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-30 23:23:42 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,12 +15,16 @@ class String
   end
 
   # call method on a string (self) and get back a count of sentences in the string
+  # 1. take each string, split it into individual sentences
+  # 2. add sentences to an array
+  # 3. count each element in that array
   # "one. two. three?"
   # "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
   def count_sentences
-    string.each do |sentence|
+    # self.each do |full_string|
       array_of_strings = []
-      elements = self.split("." || "?" || "!")
+      # elements = self.split("." || "?" || "!")
+      elements = self.split(".") || self.split("?") || self.split("!")
       array_of_strings << elements
 
       array_of_strings.count
