0dd5b8be0b3496a514a5c8c5d1e5a7199eedeeae
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-30 23:57:09 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   # call method on a string (self) and get back a count of sentences in the string
-  # split it into individual sentences; reject any strings that are empty
+  # split it into individual sentences by any character; reject any strings that are empty
   def count_sentences
         self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
