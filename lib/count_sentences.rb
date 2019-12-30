1e7f45656defe5782f92c9956f1d793faf2f08b7
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-30 23:13:33 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+  # call method on a string (self) and get back a count of sentences in the string
+  # "one. two. three?"
+  # "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
   def count_sentences
+      array_of_strings = []
+      elements = self.split("." || "?" || "!")
+      binding.pry
+      array_of_strings << elements
 
+      # return array_of_strings.count
   end
+
 end
